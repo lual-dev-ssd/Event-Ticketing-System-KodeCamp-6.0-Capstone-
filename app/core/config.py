85a483eb21125e20f99fb62f95808c52f9a8d818
David Lual <daveben.ajak@gmail.com>
2026-08-27 18:50:01 +0300

    Updated Readme and Added API Documentation

--- app/core/config.py
+++ app/core/config.py
@@ -10,12 +10,27 @@ class Settings(BaseSettings):
 
     DATABASE_URL: str = "postgresql+psycopg2://user:password@host/database_name"
 
+    SMTP_HOST: str = "smtp.gmail.com"
+    SMTP_PORT: int = 587
+    SMTP_USER: str = "daveben.ajak@gmail.com"
+    SMTP_PASSWORD: str = ""
+    SMTP_TLS: bool = True
+    EMAILS_FROM_EMAIL: str = "daveben.ajak@gmail.com"
+    EMAILS_FROM_NAME: str = "Event Ticketing System"
 
-    SECRET_KEY:str = "689794011b3717ea6923a3c2ab376f4711817c71fc5fa45228d43c009994aade"
+    SECRET_KEY:str = "Key here"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
 
-    model_config = SettingsConfigDict(env_file=".env")
+    MOMO_SUBSCRIPTION_KEY:str ="sub_key"
+    MOMO_API_USER:str ="your_momo_api_user_uuid"
+    MOMO_API_KEY:str = "your_momo_api_key"
+    MOMO_ENVIRONMENT:str ="sandbox"
+    MOMO_BASE_URL:str = "https://sandbox.momodevelopment.mtn.com"
+
+    PUBLIC_URL:str = "https://abc123.ngrok-free.dev"
+
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
 
 settings = Settings()
